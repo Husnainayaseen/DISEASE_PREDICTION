5388036c1aa0777b8c01c7f4fab68a9408c02187
husnain yaseen <husnainyaseen820@gmail.com>
2022-08-24 05:44:56 -0700

    new changes

---
+++ DISEASE_PREDICTION/Controllers/firstaidController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DISEASE_PREDICTION.Controllers
+{
+    public class firstaidController : Controller
+    {
+        // GET: firstaid
+        public ActionResult Incident()
+        {
+            return View();
+        }
+    }
+}
